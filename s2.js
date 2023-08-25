ef8fe5a01f3082185e52bafe2e7964be11655e46
sekirov <140618940+sekirov@users.noreply.github.com>
2023-08-25 13:03:48 +0300

    Update s2.js

--- s2.js
+++ s2.js
@@ -1,11 +1,15 @@
+// Wrap the code in a ready function to ensure it executes when the DOM is fully loaded
 $(document).ready(function() {
-    var HORIZONTAL_MENU_BUTTONS_SELECTOR = '.primary-nav';
+    // Selector for the horizontal menu container
+    var HORIZONTAL_MENU_CONTAINER_SELECTOR = '.header-container nav .primary-nav';
 
+    // HTML code for the new menu section
     var newMenuSection = `
         <li class="nav-item nav-item-new-section inlb ">
             <a class="nav-item-url" href="#new-section"> New Section</a>
         </li>
     `;
 
-    $(HORIZONTAL_MENU_BUTTONS_SELECTOR).append(newMenuSection);
+    // Add the new menu section to the horizontal menu
+    $(HORIZONTAL_MENU_CONTAINER_SELECTOR).append(newMenuSection);
 });
